35b20a5aa9132d72062d011dcee6d64b16eff7f0
krillshrimpp <roqkf.lhs@gmail.com>
2023-09-17 16:28:16 +0900

    start

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-from enum import Enum
 from fastapi import FastAPI
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
 
 app = FastAPI()
 
-item_db = [{"item_name": "Foo"}, {"item_name": "Bar"}]
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, quantity: int | None = None, short: bool = False):
-    item = {"item_id": item_id}    
-    if quantity:
-        item.update({"quantity": quantity})
-    if not short:
-        item.update({"short": "short"})
-    return item
+@app.get("/hello")
+async def helloworld():
+    return {"message": "Hello World"}
